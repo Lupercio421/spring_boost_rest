8a903b93605a8affa935ccde61ac219e4fa37d6a
Daniel L <daniel.llupercio@gmail.com>
2023-12-04 20:52:01 -0500

    adding the cancel and complete methods as the delete and put mappings

--- src/main/java/com/example/payroll_maven/OrderController.java
+++ src/main/java/com/example/payroll_maven/OrderController.java
@@ -3,6 +3,10 @@ package com.example.payroll_maven;
 import org.apache.coyote.Response;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,6 +42,7 @@ class OrderController {
         Order order = orderRepository.findById(id)
                 .orElseThrow(() -> new OrderNotFoundException(id));
 
+
         return assembler.toModel(order);
     }
 
@@ -49,5 +54,45 @@ class OrderController {
         return ResponseEntity
                 .created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri())
                 .body(assembler.toModel(newOrder));
+    }
+
+    @DeleteMapping("/{id}/cancel")
+    ResponseEntity<?> cancel(@PathVariable Long id){
+
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if (order.getStatus() == Status.IN_PROGRESS){
+            order.setStatus(Status.CANCELLED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create() //a hypermedia-supporting error container
+                .withTitle("Mehtod not allowed")
+                .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status"));
+    }
+
+    @PutMapping("/{id}/complete")
+    ResponseEntity<?> complete(@PathVariable Long id){
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if(order.getStatus() == Status.IN_PROGRESS){
+            order.setStatus(Status.COMPLETED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+//        implements similar logic to prevent an Order status from being completed unless in the proper state
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create()
+                        .withTitle("Method not allowed")
+                        .withDetail("You can't complete an order that is in the " + order.getStatus() + " status")
+                );
     }
 }
