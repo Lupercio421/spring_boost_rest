efdb9b8fd23a54f476b1a0ce8ea4c6e8c8c8bd04
Daniel L <daniel.llupercio@gmail.com>
2023-12-04 20:57:41 -0500

    Missing the @Component annotation

--- src/main/java/com/example/payroll_maven/OrderModelAssembler.java
+++ src/main/java/com/example/payroll_maven/OrderModelAssembler.java
@@ -2,10 +2,12 @@ package com.example.payroll_maven;
 
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Component
 class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>> {
 
     // Unconditional links to single-item resource and aggregate root
